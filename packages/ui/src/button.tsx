e969ed054e33948c19536870119bcf8bf5255f04
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2025-03-25 23:05:01 -0300

    feat: iniciando configuração do repositório
    Neste commit iniciei as configurações básicas para o projeto

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -3,18 +3,18 @@
 import { ReactNode } from "react";
 
 interface ButtonProps {
-  children: ReactNode;
-  className?: string;
-  appName: string;
+	children: ReactNode;
+	className?: string;
+	appName: string;
 }
 
 export const Button = ({ children, className, appName }: ButtonProps) => {
-  return (
-    <button
-      className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
-    >
-      {children}
-    </button>
-  );
+	return (
+		<button
+			className="bg-amber-500"
+			onClick={() => alert(`Hello from your ${appName} app!`)}
+		>
+			{children}
+		</button>
+	);
 };
