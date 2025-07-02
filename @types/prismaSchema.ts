4813949503696b6aa291da711d40516aeb3629c4
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2025-07-01 23:28:20 -0300

    feat: alterações em schema de dados

--- @types/prismaSchema.ts
+++ @types/prismaSchema.ts
@@ -1,14 +1,26 @@
 import type { Prisma } from '@prisma/client'
 
+// Tipos base (sem includes explícitos, caso precise de uma versão "pura")
 export type Account = Prisma.AccountGetPayload<{}>
-
 export type Session = Prisma.SessionGetPayload<{}>
+export type VerificationToken = Prisma.VerificationTokenGetPayload<{}>
+export type PasswordResetCode = Prisma.PasswordResetCodeGetPayload<{}>
+export type PasswordResetToken = Prisma.PasswordResetTokenGetPayload<{}>
+export type SubTask = Prisma.SubTaskGetPayload<{}>
+export type TaskTag = Prisma.TaskTagGetPayload<{}>
+export type Note = Prisma.NoteGetPayload<{}>
 
-export type User = Prisma.UserGetPayload<{
+// -----------------------------------------------------------------------------
+// Tipos com Includes para dados mais completos
+// Esses tipos são úteis quando for necessário buscar dados relacionados
+// -----------------------------------------------------------------------------
+
+export type UserWithRelations = Prisma.UserGetPayload<{
   include: {
     projects: true
     tasks: true
-    comments: true
+    commentsProject: true
+    commentsTask: true
     notifications: true
     timeTracking: true
     sharedProjects: true
@@ -17,109 +29,123 @@ export type User = Prisma.UserGetPayload<{
     sessions: true
     PasswordResetCode: true
     PasswordResetToken: true
+    subscription: true
   }
 }>
 
-export type VerificationToken = Prisma.VerificationTokenGetPayload<{}>
-
-export type PasswordResetCode = Prisma.PasswordResetCodeGetPayload<{}>
-
-export type PasswordResetToken = Prisma.PasswordResetTokenGetPayload<{}>
-
-export type Project = Prisma.ProjectGetPayload<{
+export type ProjectWithRelations = Prisma.ProjectGetPayload<{
   include: {
     tasks: {
       include: {
-        comments: true,
-        tags: true,
-        subTasks: true,
-        owner: true,
-        TimeTracking: true,
-        project: true,
+        commentsTask: {
+          include: {
+            user: true
+          }
+        }
+        tags: true
+        subTasks: true
+        owner: true
+        TimeTracking: true
+        project: true
         team: true
       }
-    },
-    comments: {
+    }
+    commentsProject: { 
       include: {
-        user: true,
-        Project: true
+        user: true // Inclui o usuário do comentário do projeto
       }
-    },
-    client: true,
-    sharedWith: true,
+    }
+    client: true
+    sharedWith: true
     owner: true
     team: true
+    Briefing: true
   }
 }>
 
-export type Client = Prisma.ClientGetPayload<{
+export type ClientWithRelations = Prisma.ClientGetPayload<{
   include: {
     projects: true
+    Briefing: true
   }
 }>
 
-export type Task = Prisma.TaskGetPayload<{
+export type TaskWithRelations = Prisma.TaskGetPayload<{
   include: {
     project: true
     owner: true
     team: true
-    comments: true
+    commentsTask: {
+      include: {
+        user: true
+      }
+    }
     tags: true
-    status: true
     subTasks: true
     TimeTracking: true
   }
 }>
 
-export type SubTask = Prisma.SubTaskGetPayload<{}>
-
-export type Notification = Prisma.NotificationGetPayload<{
+export type NotificationWithUser = Prisma.NotificationGetPayload<{
   include: {
     user: true
   }
 }>
 
-export type TimeTracking = Prisma.TimeTrackingGetPayload<{
+export type TimeTrackingWithRelations = Prisma.TimeTrackingGetPayload<{
   include: {
     user: true
     task: true
   }
 }>
 
-export type Subscription = Prisma.SubscriptionGetPayload<{
+export type SubscriptionWithUser = Prisma.SubscriptionGetPayload<{
   include: {
     user: true
   }
 }>
 
-export type Team = Prisma.TeamGetPayload<{
+export type TeamWithRelations = Prisma.TeamGetPayload<{
   include: {
     projects: true
     members: true
-    tasks: true
+    Task: true
   }
 }>
 
-export type TeamMember = Prisma.TeamMemberGetPayload<{
+export type TeamMemberWithRelations = Prisma.TeamMemberGetPayload<{
   include: {
     user: true
     team: true
   }
 }>
 
-export type SharedProject = Prisma.SharedProjectGetPayload<{
+export type SharedProjectWithRelations = Prisma.SharedProjectGetPayload<{
   include: {
     user: true
     project: true
   }
 }>
 
-export type TaskTag = Prisma.TaskTagGetPayload<{}>
-
-export type Comment = Prisma.CommentGetPayload<{
+// Tipos para os novos modelos de comentários
+export type CommentProjectWithUser = Prisma.CommentProjectGetPayload<{
   include: {
-    task: true
     user: true
-    Project: true
+    Project: true // Inclui o projeto ao qual o comentário pertence
+  }
+}>
+
+export type CommentTaskWithUser = Prisma.CommentTaskGetPayload<{
+  include: {
+    user: true
+    task: true // Inclui a tarefa à qual o comentário pertence
+  }
+}>
+
+export type BriefingWithRelations = Prisma.BriefingGetPayload<{
+  include: {
+    client: true,
+    project: true,
+    notes: true
   }
 }>
