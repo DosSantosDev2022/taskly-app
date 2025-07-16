6515f2cf2254be259af85b485b572ab4eb7cf2a8
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2025-07-16 15:52:49 -0300

    feat: adiciona componente Link do next js ao componente navigation-menu

--- src/components/ui/navigation-menu.tsx
+++ src/components/ui/navigation-menu.tsx
@@ -1,8 +1,8 @@
 import * as NavigationMenuPrimitive from "@radix-ui/react-navigation-menu";
 import { cva } from "class-variance-authority";
 import { ChevronDownIcon } from "lucide-react";
-import type * as React from "react";
-
+import Link from "next/link";
+import * as React from "react";
 import { cn } from "@/lib/utils";
 
 function NavigationMenu({
@@ -121,21 +121,26 @@ function NavigationMenuViewport({
 	);
 }
 
-function NavigationMenuLink({
-	className,
-	...props
-}: React.ComponentProps<typeof NavigationMenuPrimitive.Link>) {
+const NavigationMenuLink = React.forwardRef<
+	React.ElementRef<typeof NavigationMenuPrimitive.Link>,
+	React.ComponentProps<typeof Link>
+>(({ className, children, ...props }, ref) => {
 	return (
-		<NavigationMenuPrimitive.Link
-			data-slot="navigation-menu-link"
-			className={cn(
-				"data-[active=true]:focus:bg-accent data-[active=true]:hover:bg-accent data-[active=true]:bg-accent/50 data-[active=true]:text-accent-foreground hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus-visible:ring-ring/50 [&_svg:not([class*='text-'])]:text-muted-foreground flex flex-col gap-1 rounded-sm p-2 text-sm transition-all outline-none focus-visible:ring-[3px] focus-visible:outline-1 [&_svg:not([class*='size-'])]:size-4",
-				className,
-			)}
-			{...props}
-		/>
+		<NavigationMenuPrimitive.Link asChild>
+			<Link
+				ref={ref}
+				className={cn(
+					"data-[active=true]:focus:bg-accent data-[active=true]:hover:bg-accent data-[active=true]:bg-accent/50 data-[active=true]:text-accent-foreground hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus-visible:ring-ring/50 [&_svg:not([class*='text-'])]:text-muted-foreground flex flex-col gap-1 rounded-sm p-2 text-sm transition-all outline-none focus-visible:ring-[3px] focus-visible:outline-1 [&_svg:not([class*='size-'])]:size-4",
+					className,
+				)}
+				{...props}
+			>
+				{children}
+			</Link>
+		</NavigationMenuPrimitive.Link>
 	);
-}
+});
+NavigationMenuLink.displayName = "NavigationMenuLink";
 
 function NavigationMenuIndicator({
 	className,
